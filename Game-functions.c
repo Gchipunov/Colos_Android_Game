50a466c73875358e59925cd18dfbf3af5041960d
int 21jh <145056912+Gchipunov@users.noreply.github.com>
2024-10-22 16:37:56 -0700

    Create Game-functions.c
    Init 1

---
+++ Game-functions.c
@@ -0,0 +1,19 @@
+int initUnits(int num) {
+
+static int *pUnits malloc(num);
+for(int i = 0; i < num; i++)
+{
+if(pUnits)
+{
+pUnits[i] =i ;
+// todo add unit structure
+}
+}
+
+drawmainmenu
+
+prepareloadapp
+
+drawgameupdateframe
+
+endgame
