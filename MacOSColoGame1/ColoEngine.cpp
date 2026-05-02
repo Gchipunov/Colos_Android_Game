cfc0b436d602cf26449b9b453ff44514711683ac
int 21jh <145056912+Gchipunov@users.noreply.github.com>
2026-05-02 05:58:58 -0700

    Create ColoEngine.cpp

---
+++ MacOSColoGame1/ColoEngine.cpp
@@ -0,0 +1,19 @@
+#include "ColoEngine.h"
+
+void ColoEngine::buildBuffers()
+{
+  const std::vector<float> triangle = {
+    -0.5f, 0.5f, 0.0f,
+    0.5f, 0.5f, 0.0f,
+    0.0f, -1.0f, 0.0f,
+// -0.5f, -1.0f, 0.0f,
+  
+  };
+  
+  const std::vector<ushort> indices = {
+0 , 1, 2,
+
+  // 2 , 3 , 0 or 0, 2 ,3 
+  };
+  
+}
