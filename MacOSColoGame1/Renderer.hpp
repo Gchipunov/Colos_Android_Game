435580a2a799bf879c32277e9ef902b9de339af6
int 21jh <145056912+Gchipunov@users.noreply.github.com>
2026-05-29 11:04:30 -0700

    Refactor Renderer.hpp to include index buffer

--- MacOSColoGame1/Renderer.hpp
+++ MacOSColoGame1/Renderer.hpp
@@ -14,11 +14,25 @@ public:
     
     // The main draw call triggered by RendererAdapter
     void drawFrame(const CA::MetalDrawable* pDrawable, MTL::RenderPassDescriptor* pRpd);
-
+/*
 private:
     MTL::Device* _pDevice;
     MTL::CommandQueue* _pCommandQueue;
     MTL::Buffer* _pVertexBuffer; // For [[buffer(5)]]
     MTL::Buffer* _pTimerBuffer;  // For [[buffer(7)]]
     MTL::RenderPipelineState* _pPipelineState;
+
+*/
+private:
+    MTL::Device* _pDevice;
+    MTL::CommandQueue* _pCommandQueue;
+    
+    // Track separate vertex and index buffers
+    MTL::Buffer* _pVertexBuffer; 
+    MTL::Buffer* _pIndexBuffer;
+    uint32_t     _indexCount; // Keep track of how many indices to draw
+
+    MTL::Buffer* _pTimerBuffer;  
+    MTL::RenderPipelineState* _pPipelineState;
+
 };
